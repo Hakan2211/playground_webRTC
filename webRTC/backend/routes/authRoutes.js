a6150e8e9c3b3b68411765d990802148bf257c53
Hakan Bilgic <hbilgic1992@gmail.com>
2022-05-01 08:46:37 +0200

    add joi-validator

--- webRTC/backend/routes/authRoutes.js
+++ webRTC/backend/routes/authRoutes.js
@@ -1,10 +1,26 @@
 import express from "express";
 import { register, login } from "../controllers/auth/authController.js";
+import Joi from "joi";
+import Validator from "express-joi-validation";
+
+const validator = Validator.createValidator({});
+
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(12).required(),
+  password: Joi.string().min(6).max(12).required(),
+  mail: Joi.string().email().required(),
+});
+
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).max(12).required(),
+  mail: Joi.string().email().required(),
+});
 
 const router = express.Router();
 
-router.route("/login").post(login);
-router.route("/register").post(register);
+router.route("/login").post(validator.body(loginSchema), login);
+router.route("/register").post(validator.body(registerSchema), register);
+//router.post("/register", validator.body(registerSchema), register);
 router.route("/logout").post(register);
 
 export default router;
